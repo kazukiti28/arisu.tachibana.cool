15f98e58b32e758f6bbd9f1eb8fd635da20a670b
Kazuki F <Anzu@Anzu.local>
2016-11-06 20:02:35 +0900

    Revise
    ・画像からWebフォント読み込みに変更
    ・アニメーションの微調整
    ・携帯端末で閲覧時のバランスが悪かったので少し直そうとした(完全にはなおっていない)
    ・家具画像差し替え
    ・コード整形
    ・Alice→Arisu完全修正

--- 今日作ったやつ/js/click.js
+++ 今日作ったやつ/js/click.js
@@ -1,28 +1,28 @@
 function imgClick() {
 	$("#main").animate({
 		'top':'-1000px'
-	});//箱制御
+	});
 	$(".first").fadeOut(500);
 
 	$(".crack").fadeIn(1000);
-	$('.crack').animate({"left": "-=720px", "top": "390px"}, 1400);//クラッカー制御
-
-
+	$('.crack').animate({
+		"left": "-=720px", "top": "270px"
+	},1400);
 	setTimeout(function(){
 		$(".flipcrack").fadeIn(1000);
-		$('.flipcrack').animate({"left": "720px", "top": "390px"}, 1200);//クラッカー制御
+		$('.flipcrack').animate({
+			"left": "720px", "top": "270px"
+		},1200);
 	},1500);
-
-	$("#main").fadeOut(1);//後処理
+	$("#main").fadeOut(1);
 	setTimeout(function(){
-		$(".ichigoalice").fadeIn(300);
+		$(".ichigoarisu").fadeIn(300);
 	},3000);
 	setTimeout(function(){
 		$(".ichigo").fadeIn(300);
 	},2000);
-	//右側に出す
 	setTimeout(function(){
-		$(".mess").fadeIn(300);
+		$(".mess").fadeIn(1000);
 	},1000);
 		setTimeout(function(){
 		$(".message").fadeIn(600);
